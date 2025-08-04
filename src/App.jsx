1240b0c736b543b3169f8e106c8c7ef9f57cfb05
jesh-dev <jeshrunlaw@gmail.com>
2025-08-04 12:56:41 +0100

    Fix import order in App.jsx for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@
 import { BrowserRouter , Routes, Route } from "react-router-dom";
 import CartPage from "./Pages/Cart";
 import Home from "./Home";
-import ProductPage from "./Pages/AllProducts/Components/Grid";
 import { Toaster } from "react-hot-toast";
 import About from "./Pages/About/About";
 import Categories from "./Pages/AllProducts/Components/Categories";
+import ProductPage from "./Pages/AllProducts/Components/Grid";
 
 
 export default function App() {
